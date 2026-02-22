64c0fcb2d0bbcf5f2ec59b69a80219252aa08054
George <g.papageorgiou005@gmail.com>
2026-02-22 22:58:12 +0000

    initialize Express server with cors and dotenv

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
